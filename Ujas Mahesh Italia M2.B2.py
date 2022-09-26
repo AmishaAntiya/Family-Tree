24ee53633f65e08a482a72036fa3f9d5e811cdc7
ujasitalia <ujasitalia216@gmail.com>
2022-09-25 23:22:38 -0400

    calling script, Person, Fam, date calculation code

--- Ujas Mahesh Italia M2.B2.py
+++ Ujas Mahesh Italia M2.B2.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+from tabulate import tabulate
+from datetime import date
+
 with open('./M2.B2 GEDCOM FILE.ged', 'r+') as f:
     lines = f.read().split('\n')
 f.close()
@@ -17,16 +21,62 @@ for i in range(len(copy)):
             copy[i] = ' '.join(temp)
 
 
-with open('./M2.B2 Output.txt', 'a') as b:
-    for i in range(len(lines)):
-        b.write("{}\n".format(lines[i]))
-        temp = copy[i].split(' ')
-        if len(temp) == 2:
-            valid = 'Y' if temp[1] in validTags else 'N'
-            b.write("{}|{}|{}\n".format(temp[0], temp[1], valid))
-            continue
-        valid = 'Y' if temp[1] in validTags else 'N'
-        b.write("{}|{}|{}|{}\n".format(
-            temp[0], temp[1], valid, ' '.join(temp[2:])))
+person = []
+fams = []
 
-    
+
+class Person:
+    def __init__(self, id):
+        self.id = id.replace('@', '')
+        self.name = None
+        self.sex = None
+        self.spouse_id = "NA"
+        self.child_id = "NA"
+        self.birth = None
+        self.death = None
+        self.age = None
+
+
+class Fam:
+    def __init__(self, f_id):
+        self.f_id = f_id.replace('@', '')
+        self.marriage = None
+        self.divorce = None
+        self.husband = None
+        self.wife = None
+        self.children = []
+
+
+def calc_date(obj, line, dType):
+    if line[1] == "DATE" and line[0] == "2":
+        if isinstance(obj, Person):
+            if dType == "BIRT":
+                obj.birth = datetime.strptime(
+                    line[2].rstrip(), '%d %b %Y').date()
+            elif dType == "DEAT":
+                obj.death = datetime.strptime(
+                    line[2].rstrip(), '%d %b %Y').date()
+        elif isinstance(obj, Fam):
+            if dType == 0:
+                obj.marriage = datetime.strptime(
+                    line[2].rstrip(), '%d %b %Y').date()
+            elif dType == 1:
+                obj.divorce = datetime.strptime(
+                    line[2].rstrip(), '%d %b %Y').date()
+
+
+i = 0
+while len(copy) > i:
+    line = copy[i].split(" ")
+    if line[1] == "INDI" and len(line) != 2:
+        if line[1] == "INDI":
+            calc_individual(copy, i+1, Person(line[2].rstrip()))
+    elif line[1].rstrip() == "FAM":
+        calc_family(copy, i+1, Fam(line[2].rstrip()))
+    i += 1
+
+person.sort(key=lambda y: int(y.id[1:]))
+fams.sort(key=lambda z: int(z.f_id[1:]))
+
+print_individuals()
+print_families()
